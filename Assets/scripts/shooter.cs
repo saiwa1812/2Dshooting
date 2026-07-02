996fd7c9a251fe5c28b96609636af30b612960a8
saiwa1812 <akiro1812@gmail.com>
2026-07-03 01:31:27 +0900

    shooter.cs

--- Assets/scripts/shooter.cs
+++ Assets/scripts/shooter.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class shooter : MonoBehaviour
 {
@@ -11,15 +12,28 @@ public class shooter : MonoBehaviour
     // 0: 待機
     // 1: 攻撃
 
+    private float speed = 10f;
+    private InputAction moveAction;
+    private InputAction shootAction;
+
+
     public void awake(){
-        spriteRenderer.sprite = idleSprite;
+        
+    }
+
+    private void Start(){
+        // "Move" と "Shoot" のリファレンスを探す
+        moveAction = InputSystem.actions.FindAction("Move");
+        shootAction = InputSystem.actions.FindAction("Shoot");
+        // 画像を idleに
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = idleSprite; 
     }
 
     public void ChangeState(int newState){
         state = newState;
 
-        switch (state)
-        {
+        switch (state){
             case 0:
                 spriteRenderer.sprite = idleSprite;
                 break;
@@ -27,5 +41,22 @@ public class shooter : MonoBehaviour
                 spriteRenderer.sprite = attackSprite;
                 break;
         }
+    }
+
+    private void Update()
+    {
+        // 移動処理
+        var moveValue = moveAction.ReadValue<Vector2>();
+        var move = new Vector2(moveValue.x, moveValue.y) * speed * Time.deltaTime;
+        transform.Translate(move);
+        // ジャンプ処理
+        Debug.Log("shoot is null");
+        if (shootAction.IsPressed()){
+            ChangeState(1);
+        }
+        else
+        {
+            ChangeState(0);
+        }   
     }
 }
